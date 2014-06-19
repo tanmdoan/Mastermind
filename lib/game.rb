53a06a8d09b01d4975e3047ecc4ec0e718cce122
Tan Doan <tdoan87@gmail.com>
2014-06-18 19:06:56 -0600

    functional game, with some bugs

--- lib/game.rb
+++ lib/game.rb
@@ -3,36 +3,41 @@ require "./lib/guess"
 require "./lib/sequence_matcher"
 
 class Game
-  attr_reader :command, :answer
+  attr_reader :command, :answer, :guesses
   def initialize
     @command = ""
     @answer  = SequenceGenerator.new(4).generate
+    @guesses = []
   end
 
   def calculate_time(t1, t2)
     time = t2 - t1
     minutes, seconds = time.divmod(60)
-    puts "It took you #{minutes} minutes and #{seconds.to_i} seconds to be a winner."
+    puts "in #{minutes} minutes and #{seconds.to_i} seconds to be a winner."
   end
 
   def play
     puts "Let's get to guessin!"
+    puts "secretcode = " + answer.code
       initial_time = Time.new
     until command == ("q")
       print "Enter your guess: "
       @command = gets.chomp.downcase
       guess    = Guess.new(@command)
+      @guesses << guess
       validator = GuessValidator.new(guess)
       matcher = SequenceMatcher.new(guess.sequence, answer.code)
         if matcher.same
         end_time = Time.new
+        puts "Woot, you win!!!\n It took you #{@guesses.count} guess(es)"
         calculate_time(initial_time, end_time)
-        puts "Woot, you win!!!"
         break
         elsif  validator.valid?
         correct_spots = matcher.count_in_correct_spots
         correct_letters = matcher.count_correct_letters
-        puts "You have #{correct_spots} letter(s) in the right spot, out of the #{correct_letters} correct."
+        # @guesses << guess
+        puts "You have #{correct_spots} letter(s) in the right spot, out of the #{correct_letters} correct.
+        You've guessed #{guesses.count} times."
         elsif @command == "q"
           puts "Latez!"
           break
